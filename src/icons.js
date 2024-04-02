11bb6a636ed538e48ca313c66c01aad1c4990240
Abrham <abrsh6266@gmail.com>
2024-04-01 23:25:47 -0800

    creating navbar component

--- src/icons.js
+++ src/icons.js
@@ -1,17 +1,17 @@
 export const CartIcon = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6 '
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6 "
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
       <path
-        strokeLinecap='round'
-        strokeLinejoin='round'
-        d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z'
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
       />
     </svg>
   );
@@ -20,14 +20,14 @@ export const CartIcon = () => {
 export const ChevronDown = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M19 9l-7 7-7-7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M19 9l-7 7-7-7" />
     </svg>
   );
 };
@@ -35,14 +35,31 @@ export const ChevronDown = () => {
 export const ChevronUp = () => {
   return (
     <svg
-      xmlns='http://www.w3.org/2000/svg'
-      className='h-6 w-6'
-      fill='none'
-      viewBox='0 0 24 24'
-      stroke='currentColor'
+      xmlns="http://www.w3.org/2000/svg"
+      className="h-6 w-6"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
       strokeWidth={2}
     >
-      <path strokeLinecap='round' strokeLinejoin='round' d='M5 15l7-7 7 7' />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 15l7-7 7 7" />
     </svg>
   );
 };
+
+export const Testing = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    class="w-6 h-6"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z"
+    />
+  </svg>
+);
